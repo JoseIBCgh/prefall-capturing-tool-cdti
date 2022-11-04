04043191eb46ad45923a87255370a1327db4fcfa
Bernat <bernatdiaz94@gmail.com>
2022-11-04 11:07:32 +0100

    restructuracion grafos

--- ibcdatacsharp/UI/MainWindow.xaml.cs
+++ ibcdatacsharp/UI/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows;
-using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using ibcdatacsharp.DeviceList.TreeClasses;
