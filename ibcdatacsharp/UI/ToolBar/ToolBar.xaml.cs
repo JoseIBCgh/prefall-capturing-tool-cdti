5bfad47caed38f350f91ef614b025d39dd16283a
Bernat <bernatdiaz94@gmail.com>
2022-11-21 13:09:09 +0100

    efecto presionar toolbar

--- ibcdatacsharp/UI/ToolBar/ToolBar.xaml.cs
+++ ibcdatacsharp/UI/ToolBar/ToolBar.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using ibcdatacsharp.UI.ToolBar.Enums;
 using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using ibcdatacsharp.UI.Common;
+using System.Windows.Media.Animation;
 
 namespace ibcdatacsharp.UI.ToolBar
 {
@@ -10,10 +14,84 @@ namespace ibcdatacsharp.UI.ToolBar
     /// </summary>
     public partial class ToolBar : Page
     {
+        private const int ANIMATION_MS = 100;
+        private const int INITIAL_ICON_SIZE = 30;
+        private const int PRESSED_ICON_SIZE = 25;
+        private const int INITIAL_FONT_SIZE = 11;
+        private const int PRESSED_FONT_SIZE = 9;
         public ToolBar()
         {
             InitializeComponent();
             deactivateButtons();
+            initButtonAnimations();
+        }
+        private void initButtonAnimations()
+        {
+            void buttonPressed(object sender, RoutedEventArgs e)
+            {
+                Button button = (Button)sender;
+                if (button.IsEnabled)
+                {
+                    StackPanel stackPanel = Helpers.GetChildOfType<StackPanel>(button);
+                    Image image = Helpers.GetChildOfType<Image>(stackPanel);
+                    TextBlock text = Helpers.GetChildOfType<TextBlock>(stackPanel);
+
+                    DoubleAnimation imageAnimation = new DoubleAnimation();
+
+                    imageAnimation.From = INITIAL_ICON_SIZE;
+                    imageAnimation.To = PRESSED_ICON_SIZE;
+                    imageAnimation.Duration = TimeSpan.FromMilliseconds(ANIMATION_MS);
+
+                    DoubleAnimation textAnimation = new DoubleAnimation();
+
+                    textAnimation.From = INITIAL_FONT_SIZE;
+                    textAnimation.To = PRESSED_FONT_SIZE;
+                    textAnimation.Duration = TimeSpan.FromMilliseconds(ANIMATION_MS);
+
+                    image.BeginAnimation(Image.WidthProperty, imageAnimation);
+                    text.BeginAnimation(TextBlock.FontSizeProperty, textAnimation);
+                }
+            }
+            void buttonReleased(object sender, RoutedEventArgs e)
+            {
+                Button button = (Button)sender;
+                if (button.IsEnabled)
+                {
+                    StackPanel stackPanel = Helpers.GetChildOfType<StackPanel>(button);
+                    Image image = Helpers.GetChildOfType<Image>(stackPanel);
+                    TextBlock text = Helpers.GetChildOfType<TextBlock>(stackPanel);
+
+                    DoubleAnimation imageAnimation = new DoubleAnimation();
+
+                    imageAnimation.From = PRESSED_ICON_SIZE;
+                    imageAnimation.To = INITIAL_ICON_SIZE;
+                    imageAnimation.Duration = TimeSpan.FromMilliseconds(ANIMATION_MS);
+
+                    DoubleAnimation textAnimation = new DoubleAnimation();
+
+                    textAnimation.From = PRESSED_FONT_SIZE;
+                    textAnimation.To = INITIAL_FONT_SIZE;
+                    textAnimation.Duration = TimeSpan.FromMilliseconds(ANIMATION_MS);
+
+                    image.BeginAnimation(Image.WidthProperty, imageAnimation);
+                    text.BeginAnimation(TextBlock.FontSizeProperty, textAnimation);
+                }
+            }
+            List<Button> buttons = new List<Button>();
+            buttons.Add(scan);
+            buttons.Add(connect);
+            buttons.Add(disconnect);
+            buttons.Add(openCamera);
+            buttons.Add(record);
+            buttons.Add(capture);
+            buttons.Add(pause);
+            buttons.Add(stop);
+            buttons.Add(capturedFiles);
+            foreach (Button button in buttons)
+            {
+                button.PreviewMouseLeftButtonDown += buttonPressed;
+                button.PreviewMouseLeftButtonUp += buttonReleased;
+            }
         }
         private void deactivateButtons()
         {
