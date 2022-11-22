689fca9a7ae8b436d27df191982be3668ba0c6cd
Bernat <bernatdiaz94@gmail.com>
2022-11-22 13:40:35 +0100

    aceptar , y . csv

--- ibcdatacsharp/UI/Graphs/GraphData.cs
+++ ibcdatacsharp/UI/Graphs/GraphData.cs
@@ -1,5 +1,6 @@
 ﻿using OpenCvSharp;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace ibcdatacsharp.UI.Graphs
 {
@@ -53,17 +54,17 @@ namespace ibcdatacsharp.UI.Graphs
         public FrameData(string csvLine)
         {
             string[] values = csvLine.Split(' ');
-            time = double.Parse(values[1]);
+            time = double.Parse(values[1], CultureInfo.InvariantCulture);
             frame = int.Parse(values[2]);
-            accX = double.Parse(values[3]);
-            accY = double.Parse(values[4]);
-            accZ = double.Parse(values[5]);
-            gyrX = double.Parse(values[6]);
-            gyrY = double.Parse(values[7]);
-            gyrZ = double.Parse(values[8]);
-            magX = double.Parse(values[9]);
-            magY = double.Parse(values[10]);
-            magZ = double.Parse(values[11]);
+            accX = double.Parse(values[3], CultureInfo.InvariantCulture);
+            accY = double.Parse(values[4], CultureInfo.InvariantCulture);
+            accZ = double.Parse(values[5], CultureInfo.InvariantCulture);
+            gyrX = double.Parse(values[6], CultureInfo.InvariantCulture);
+            gyrY = double.Parse(values[7], CultureInfo.InvariantCulture);
+            gyrZ = double.Parse(values[8], CultureInfo.InvariantCulture);
+            magX = double.Parse(values[9], CultureInfo.InvariantCulture);
+            magY = double.Parse(values[10], CultureInfo.InvariantCulture);
+            magZ = double.Parse(values[11], CultureInfo.InvariantCulture);
         }
     }
 }
