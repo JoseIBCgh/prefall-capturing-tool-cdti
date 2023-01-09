09c8a54331f18562448ec829cf97d55c6b2a45dd
Bernat <bernatdiaz94@gmail.com>
2023-01-09 12:14:05 +0100

    replay label bug

--- ibcdatacsharp/UI/Graphs/Model4S.cs
+++ ibcdatacsharp/UI/Graphs/Model4S.cs
@@ -143,7 +143,7 @@ namespace ibcdatacsharp.UI.Graphs
                 plot.Plot.Remove(signalPlotX);
                 plot.Plot.Remove(signalPlotY);
                 plot.Plot.Remove(signalPlotZ);
-                signalPlotW = plot.Plot.AddSignal(valuesW, color: wColor, label: "X");
+                signalPlotW = plot.Plot.AddSignal(valuesW, color: wColor, label: "W");
                 signalPlotX = plot.Plot.AddSignal(valuesX, color: xColor, label: "X");
                 signalPlotY = plot.Plot.AddSignal(valuesY, color: yColor, label: "Y");
                 signalPlotZ = plot.Plot.AddSignal(valuesZ, color: zColor, label: "Z");
