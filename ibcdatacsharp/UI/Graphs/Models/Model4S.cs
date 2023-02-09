6f9e0c1b6303a4271d2ff125a8852257f2bb3e63
Bernat <bernatdiaz94@gmail.com>
2023-02-09 15:12:32 +0100

    #112 v1.0.0_sagital_angles

--- ibcdatacsharp/UI/Graphs/Models/Model4S.cs
+++ ibcdatacsharp/UI/Graphs/Models/Model4S.cs
@@ -38,15 +38,22 @@ namespace ibcdatacsharp.UI.Graphs.Models
         private Color yColor = Config.colorY;
         private Color zColor = Config.colorZ;
 
+        private CaptureModel captureModel;
+        private ReplayModel replayModel;
+
         public Model4S(WpfPlot plot, double minY, double maxY, string title = "", string units = "")
         {
+            captureModel = new CaptureModel(this);
+            replayModel = new ReplayModel(this);
+
             this.minY = minY;
             this.maxY = maxY;
             this.plot = plot;
 
+            plot.Plot.SetAxisLimitsY(yMin: minY, yMax: maxY);
+
             lineFrame = plot.Plot.AddVerticalLine(0, color: frameColor, width: verticalLineWidth, style: LineStyle.Dash);
 
-            plot.Plot.SetAxisLimitsX(xMin: 0, MAX_POINTS);
             plot.Plot.Legend(location: Alignment.UpperRight);
 
 
@@ -66,65 +73,19 @@ namespace ibcdatacsharp.UI.Graphs.Models
         }
         public void initCapture()
         {
-            valuesW = new double[CAPACITY];
-            valuesX = new double[CAPACITY];
-            valuesY = new double[CAPACITY];
-            valuesZ = new double[CAPACITY];
-            plot.Plot.Remove(signalPlotW);
-            plot.Plot.Remove(signalPlotX);
-            plot.Plot.Remove(signalPlotY);
-            plot.Plot.Remove(signalPlotZ);
-            signalPlotW = plot.Plot.AddSignal(valuesW, color: wColor, label: "W");
-            signalPlotX = plot.Plot.AddSignal(valuesX, color: xColor, label: "X");
-            signalPlotY = plot.Plot.AddSignal(valuesY, color: yColor, label: "Y");
-            signalPlotZ = plot.Plot.AddSignal(valuesZ, color: zColor, label: "Z");
-            signalPlotW.MarkerSize = 0;
-            signalPlotX.MarkerSize = 0;
-            signalPlotY.MarkerSize = 0;
-            signalPlotZ.MarkerSize = 0;
-            plot.Plot.SetAxisLimitsY(yMin: minY, yMax: maxY);
-            nextIndex = 0;
-            maxRenderIndex = nextIndex;
+            captureModel.initCapture();
         }
         #region Replay
         // Añade todos los datos de golpe (solo para replay)
         public void updateData(double[] w, double[] x, double[] y, double[] z)
         {
-            valuesW = w;
-            valuesX = x;
-            valuesY = y;
-            valuesZ = z;
-            plot.Plot.Remove(signalPlotW);
-            plot.Plot.Remove(signalPlotX);
-            plot.Plot.Remove(signalPlotY);
-            plot.Plot.Remove(signalPlotZ);
-            signalPlotW = plot.Plot.AddSignal(valuesW, color: wColor, label: "W");
-            signalPlotX = plot.Plot.AddSignal(valuesX, color: xColor, label: "X");
-            signalPlotY = plot.Plot.AddSignal(valuesY, color: yColor, label: "Y");
-            signalPlotZ = plot.Plot.AddSignal(valuesZ, color: zColor, label: "Z");
-            signalPlotW.MarkerSize = 0;
-            signalPlotX.MarkerSize = 0;
-            signalPlotY.MarkerSize = 0;
-            signalPlotZ.MarkerSize = 0;
-            maxRenderIndex = 0;
-            //plot.Plot.SetAxisLimitsX(xMin: 0, xMax: Math.Min(MAX_POINTS, valuesX.Length));
-            plot.Plot.SetAxisLimitsX(xMin: 0, xMax: valuesX.Length);
-            plot.Plot.SetAxisLimitsY(yMin: minY, yMax: maxY);
+            clear();
+            replayModel.updateData(w, x, y, z);
         }
         // Cambia los datos a mostrar
         public void updateIndex(int index)
         {
-            index = Math.Min(index, valuesX.Length); //Por si acaso
-            maxRenderIndex = index;
-
-            signalPlotW.Label = "W= " + valuesW[index].ToString("0.##");
-            signalPlotX.Label = "X= " + valuesX[index].ToString("0.##");
-            signalPlotY.Label = "Y= " + valuesY[index].ToString("0.##");
-            signalPlotZ.Label = "Z= " + valuesZ[index].ToString("0.##");
-
-            //plot.Plot.SetAxisLimits(xMin: Math.Max(0, index - MAX_POINTS),
-            //    xMax: Math.Max(index + RIGHT_SEPARATION, Math.Min(MAX_POINTS, valuesX.Length)));
-            plot.Render();
+            replayModel.updateIndex(index);
         }
         #endregion Replay
         // Esta version funciona mejor pero usa mas memoria. Si se sobrepasa la memoria incial hay que modificar el tamaño de las arrays.
@@ -132,112 +93,156 @@ namespace ibcdatacsharp.UI.Graphs.Models
         // Actualiza los datos
         public async void updateData(double[] data)
         {
-            if (nextIndex >= CAPACITY) // No deberia pasar
-            {
-                CAPACITY = CAPACITY * GROW_FACTOR;
-                Array.Resize(ref valuesW, CAPACITY);
-                Array.Resize(ref valuesX, CAPACITY);
-                Array.Resize(ref valuesY, CAPACITY);
-                Array.Resize(ref valuesZ, CAPACITY);
-                plot.Plot.Remove(signalPlotW);
-                plot.Plot.Remove(signalPlotX);
-                plot.Plot.Remove(signalPlotY);
-                plot.Plot.Remove(signalPlotZ);
-                signalPlotW = plot.Plot.AddSignal(valuesW, color: wColor, label: "W");
-                signalPlotX = plot.Plot.AddSignal(valuesX, color: xColor, label: "X");
-                signalPlotY = plot.Plot.AddSignal(valuesY, color: yColor, label: "Y");
-                signalPlotZ = plot.Plot.AddSignal(valuesZ, color: zColor, label: "Z");
-                signalPlotW.MarkerSize = 0;
-                signalPlotX.MarkerSize = 0;
-                signalPlotY.MarkerSize = 0;
-                signalPlotZ.MarkerSize = 0;
-            }
-            valuesW[nextIndex] = data[0];
-            valuesX[nextIndex] = data[1];
-            valuesY[nextIndex] = data[2];
-            valuesZ[nextIndex] = data[3];
-            signalPlotW.Label = "W= " + data[0].ToString("0.##");
-            signalPlotX.Label = "X= " + data[1].ToString("0.##");
-            signalPlotY.Label = "Y= " + data[2].ToString("0.##");
-            signalPlotZ.Label = "Z= " + data[3].ToString("0.##");
-            nextIndex++;
+            captureModel.updateData(data);
         }
         public async void updateData(Quaternion[] data, bool render = true)
         {
-            if (nextIndex + data.Length >= CAPACITY) // No deberia pasar
-            {
-                CAPACITY = CAPACITY * GROW_FACTOR;
-                Array.Resize(ref valuesW, CAPACITY);
-                Array.Resize(ref valuesX, CAPACITY);
-                Array.Resize(ref valuesY, CAPACITY);
-                Array.Resize(ref valuesZ, CAPACITY);
-                plot.Plot.Remove(signalPlotW);
-                plot.Plot.Remove(signalPlotX);
-                plot.Plot.Remove(signalPlotY);
-                plot.Plot.Remove(signalPlotZ);
-                signalPlotW = plot.Plot.AddSignal(valuesW, color: wColor, label: "W");
-                signalPlotX = plot.Plot.AddSignal(valuesX, color: xColor, label: "X");
-                signalPlotY = plot.Plot.AddSignal(valuesY, color: yColor, label: "Y");
-                signalPlotZ = plot.Plot.AddSignal(valuesZ, color: zColor, label: "Z");
-                signalPlotW.MarkerSize = 0;
-                signalPlotX.MarkerSize = 0;
-                signalPlotY.MarkerSize = 0;
-                signalPlotZ.MarkerSize = 0;
-            }
-            for (int i = 0; i < data.Length; i++)
-            {
-                valuesW[nextIndex + i] = data[i].W;
-                valuesX[nextIndex + i] = data[i].X;
-                valuesY[nextIndex + i] = data[i].Y;
-                valuesZ[nextIndex + i] = data[i].Z;
-            }
-            signalPlotW.Label = "W= " + data[data.Length - 1].W.ToString("0.##");
-            signalPlotX.Label = "X= " + data[data.Length - 1].X.ToString("0.##");
-            signalPlotY.Label = "Y= " + data[data.Length - 1].Y.ToString("0.##");
-            signalPlotZ.Label = "Z= " + data[data.Length - 1].Z.ToString("0.##");
-            nextIndex += data.Length;
-            if (render)
-            {
-                this.render();
-            }
+            captureModel.updateData(data, render);
         }
         // Actualiza el renderizado
         public async void render()
         {
-            int index = nextIndex - 1;
-            if (index < 0)
-            {
-                index = 0;
-            }
-            maxRenderIndex = index;
-            plot.Plot.SetAxisLimits(xMin: Math.Max(0, index - MAX_POINTS),
-                xMax: Math.Max(index + RIGHT_SEPARATION, Math.Min(MAX_POINTS, valuesX.Length)));
             plot.Render();
         }
         // Borra todos los puntos de todas las lineas
         public void clear()
         {
-            nextIndex = 0;
-            maxRenderIndex = nextIndex;
+            plot.Plot.Clear(typeof(SignalPlot));
             plot.Render();
         }
-        // Usar esto para actualizar la line tambien
-        private int maxRenderIndex
+        class CaptureModel
         {
-            get
+            Model4S model;
+            private const int CAPACITY = 200;
+            double[] valuesX;
+            double[] valuesY;
+            double[] valuesZ;
+            double[] valuesW;
+            SignalPlot signalPlotX;
+            SignalPlot signalPlotY;
+            SignalPlot signalPlotZ;
+            SignalPlot signalPlotW;
+            private int nextIndex = 0;
+
+            public CaptureModel(Model4S model)
             {
-                return (int)lineFrame.X;
+                this.model = model;
             }
-            set
+            public void initCapture()
             {
-                signalPlotW.MaxRenderIndex = value;
-                signalPlotX.MaxRenderIndex = value;
-                signalPlotY.MaxRenderIndex = value;
-                signalPlotZ.MaxRenderIndex = value;
-                lineFrame.X = value;
-                //lineX.Y = valuesX[value];
-                //lineY.Y = valuesY[value];
-                //lineZ.Y = valuesZ[value];
+                valuesX = new double[CAPACITY];
+                valuesY = new double[CAPACITY];
+                valuesZ = new double[CAPACITY];
+                valuesW = new double[CAPACITY];
+                signalPlotX = model.plot.Plot.AddSignal(valuesX, color: model.xColor, label: "X");
+                signalPlotY = model.plot.Plot.AddSignal(valuesY, color: model.yColor, label: "Y");
+                signalPlotZ = model.plot.Plot.AddSignal(valuesZ, color: model.zColor, label: "Z");
+                signalPlotW = model.plot.Plot.AddSignal(valuesW, color: model.wColor, label: "W");
+                signalPlotX.MarkerSize = 0;
+                signalPlotY.MarkerSize = 0;
+                signalPlotZ.MarkerSize = 0;
+                signalPlotW.MarkerSize = 0;
+                nextIndex = 0;
+                model.plot.Plot.SetAxisLimitsX(xMin: 0, xMax: CAPACITY);
+            }
+            public void updateData(Quaternion[] data, bool render = true)
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    int index = (nextIndex + i) % CAPACITY;
+                    valuesW[index] = data[i].W;
+                    valuesX[index] = data[i].X;
+                    valuesY[index] = data[i].Y;
+                    valuesZ[index] = data[i].Z;
+                }
+                signalPlotW.Label = "W= " + data[data.Length - 1].W.ToString("0.##");
+                signalPlotX.Label = "X= " + data[data.Length - 1].X.ToString("0.##");
+                signalPlotY.Label = "Y= " + data[data.Length - 1].Y.ToString("0.##");
+                signalPlotZ.Label = "Z= " + data[data.Length - 1].Z.ToString("0.##");
+                nextIndex += data.Length;
+                model.lineFrame.X = nextIndex % CAPACITY;
+                if (render)
+                {
+                    model.plot.Render();
+                }
+            }
+            public void updateData(double[] data)
+            {
+                valuesW[nextIndex] = data[0];
+                valuesX[nextIndex] = data[1];
+                valuesY[nextIndex] = data[2];
+                valuesZ[nextIndex] = data[3];
+                signalPlotW.Label = "W= " + data[0].ToString("0.##");
+                signalPlotX.Label = "X= " + data[1].ToString("0.##");
+                signalPlotY.Label = "Y= " + data[2].ToString("0.##");
+                signalPlotZ.Label = "Z= " + data[3].ToString("0.##");
+                nextIndex++;
+                model.lineFrame.X = nextIndex % CAPACITY;
+                model.plot.Render();
+            }
+        }
+        class ReplayModel
+        {
+            Model4S model;
+            double[] valuesX;
+            double[] valuesY;
+            double[] valuesZ;
+            double[] valuesW;
+            SignalPlot signalPlotX;
+            SignalPlot signalPlotY;
+            SignalPlot signalPlotZ;
+            SignalPlot signalPlotW;
+            public ReplayModel(Model4S model)
+            {
+                this.model = model;
+            }
+            public void updateData(double[] w, double[] x, double[] y, double[] z)
+            {
+                valuesW = w;
+                valuesX = x;
+                valuesY = y;
+                valuesZ = z;
+                model.plot.Plot.Remove(signalPlotW);
+                model.plot.Plot.Remove(signalPlotX);
+                model.plot.Plot.Remove(signalPlotY);
+                model.plot.Plot.Remove(signalPlotZ);
+                signalPlotW = model.plot.Plot.AddSignal(valuesW, color: model.wColor, label: "W");
+                signalPlotX = model.plot.Plot.AddSignal(valuesX, color: model.xColor, label: "X");
+                signalPlotY = model.plot.Plot.AddSignal(valuesY, color: model.yColor, label: "Y");
+                signalPlotZ = model.plot.Plot.AddSignal(valuesZ, color: model.zColor, label: "Z");
+                signalPlotW.MarkerSize = 0;
+                signalPlotX.MarkerSize = 0;
+                signalPlotY.MarkerSize = 0;
+                signalPlotZ.MarkerSize = 0;
+                maxRenderIndex = 0;
+                model.plot.Plot.SetAxisLimitsX(xMin: 0, xMax: valuesX.Length);
+            }
+            public void updateIndex(int index)
+            {
+                index = Math.Min(index, valuesX.Length); //Por si acaso
+                maxRenderIndex = index;
+
+                signalPlotW.Label = "W= " + valuesW[index].ToString("0.##");
+                signalPlotX.Label = "X= " + valuesX[index].ToString("0.##");
+                signalPlotY.Label = "Y= " + valuesY[index].ToString("0.##");
+                signalPlotZ.Label = "Z= " + valuesZ[index].ToString("0.##");
+
+                model.plot.Render();
+            }
+            private int maxRenderIndex
+            {
+                get
+                {
+                    return (int)model.lineFrame.X;
+                }
+                set
+                {
+                    signalPlotW.MaxRenderIndex = value;
+                    signalPlotX.MaxRenderIndex = value;
+                    signalPlotY.MaxRenderIndex = value;
+                    signalPlotZ.MaxRenderIndex = value;
+                    model.lineFrame.X = value;
+                }
             }
         }
     }
