311ea237783be4771f526c7f87fe1ba8e45bfd4e
Bernat Díaz <bernatdiaz@gmail.com>
2023-09-07 11:29:50 +0200

    login no funciona

---
+++ ibcdatacsharp/Login/Connection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.EntityFramework;
+using MySql.Data.MySqlClient;
+using System.Windows;
+using System.Diagnostics.Eventing.Reader;
+
+namespace sign_in_dotnet_wpf;
+public class Connection
+{
+    private MySqlConnection _connection;
+    private MySqlConnectionStringBuilder _connectionStringBuilder;
+    private string server;
+    private uint port;
+    private string user;
+    private string pass;
+    private string db;
+
+
+    public Connection()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        server = "srv.ibc.bio";
+        port = 32772;
+        user = "root";
+        pass = "root";
+        db = "prefall";
+
+        _connectionStringBuilder = new MySqlConnectionStringBuilder();
+        _connectionStringBuilder.Server = server;
+        _connectionStringBuilder.Port = port;
+        _connectionStringBuilder.Database = db;
+        _connectionStringBuilder.UserID = user;
+        _connectionStringBuilder.Password = pass;
+
+        _connection = new MySqlConnection(_connectionStringBuilder.ToString());
+
+       
+
+    }
+
+    public bool OpenConnection()
+    {
+        try
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection.Open();
+
+            }
+            System.Windows.MessageBox.Show($"Connection done");
+            return true;
+
+        }
+        catch (MySqlException e)
+        {
+            switch (e.Number)
+            {
+                case 0: System.Windows.MessageBox.Show("Error" + e); break;
+                case 1045: System.Windows.MessageBox.Show($"Error: {e.Message}"); break;
+            }
+            return false;
+        }
+    }
+
+    public void CloseConnection()
+    {
+        this._connection.Close();
+    }
+
+    public MySqlConnection GetConnection()
+    {
+        return this._connection;
+    }
+}
---
+++ ibcdatacsharp/Login/Dashboard.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ibcdatacsharp.UI;
+using MySql.Data.MySqlClient;
+
+namespace sign_in_dotnet_wpf;
+/// <summary>
+/// Interaction logic for Dashboard.xaml
+/// </summary>
+public partial class Dashboard : Window
+{
+    private Connection connection;
+    private string username, password, sql;
+
+    public Dashboard()
+    {
+        InitializeComponent();
+        connection = new Connection();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+       
+        connection.OpenConnection();
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        connection.CloseConnection();
+    }
+
+    private void Button_Click_2(object sender, RoutedEventArgs e)
+    {
+        this.username = user.Text;
+        this.password = pass.Password.ToString();
+
+        sql = "SELECT username, password from users where username='" + this.username + "' AND " + "password='" + this.password +"'";
+
+        if (this.connection.OpenConnection() == true)
+        {
+            var command = new MySqlCommand(sql, this.connection.GetConnection());
+            object a = command.ExecuteScalar();
+
+            if (a != null)
+            {
+                MessageBox.Show("login successful!");
+
+                MainWindow mw = new MainWindow();
+                this.Close();
+                mw.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Error login");
+                
+            }
+        }
+
+
+    }
+}
