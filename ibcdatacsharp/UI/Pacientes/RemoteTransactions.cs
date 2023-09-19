4bbd4ede4a31d74dcce5170a65298278af8a4769
Bernat Díaz <bernatdiaz@gmail.com>
2023-09-19 13:21:38 +0200

    subir en bulk

--- ibcdatacsharp/UI/Pacientes/RemoteTransactions.cs
+++ ibcdatacsharp/UI/Pacientes/RemoteTransactions.cs
@@ -6,6 +6,8 @@ using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Text;
+using System.Windows.Forms;
 
 namespace ibcdatacsharp.UI.Pacientes
 {
@@ -69,40 +71,27 @@ namespace ibcdatacsharp.UI.Pacientes
                                         insertCommand.ExecuteNonQuery();
                                     }
                                     string line;
+                                    string tableName = "test_unit";
                                     int item = 0;
+                                    StringBuilder bulkInsertSql = new StringBuilder();
+                                    bulkInsertSql.Append($"INSERT INTO {tableName} (num_test, id_paciente, item, time, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, mag_x, mag_y, mag_z, lacc_x, lacc_y, lacc_z, quat_x, quat_y, quat_z, quat_w) VALUES ");
+
                                     while ((line = reader.ReadLine()) != null)
                                     {
                                         string[] data = line.Split(' ');
-                                        string tableName = "test_unit";
 
-                                        sql = $"INSERT INTO {tableName} (num_test, id_paciente, item, time, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, mag_x, mag_y, mag_z, lacc_x, lacc_y, lacc_z, quat_x, quat_y, quat_z, quat_w) " +
-                                                $"VALUES (@num_test, @id, @item, @time, @acc_x, @acc_y, @acc_z, @gyr_x, @gyr_y, @gyr_z, @mag_x, @mag_y, @mag_z, @lacc_x, @lacc_y, @lacc_z, @quat_x, @quat_y, @quat_z, @quat_w)";
-                                        using (MySqlCommand insertRowcommand = new MySqlCommand(sql, connection.GetConnection()))
-                                        {
-                                            insertRowcommand.Parameters.AddWithValue("@num_test", nextNumTest);
-                                            insertRowcommand.Parameters.AddWithValue("@id", id);
-                                            insertRowcommand.Parameters.AddWithValue("@item", item);
-                                            insertRowcommand.Parameters.AddWithValue("@time", data[1]);
-                                            insertRowcommand.Parameters.AddWithValue("@acc_x", data[3]);
-                                            insertRowcommand.Parameters.AddWithValue("@acc_y", data[4]);
-                                            insertRowcommand.Parameters.AddWithValue("@acc_z", data[5]);
-                                            insertRowcommand.Parameters.AddWithValue("@gyr_x", data[6]);
-                                            insertRowcommand.Parameters.AddWithValue("@gyr_y", data[7]);
-                                            insertRowcommand.Parameters.AddWithValue("@gyr_z", data[8]);
-                                            insertRowcommand.Parameters.AddWithValue("@mag_x", data[9]);
-                                            insertRowcommand.Parameters.AddWithValue("@mag_y", data[10]);
-                                            insertRowcommand.Parameters.AddWithValue("@mag_z", data[11]);
-                                            insertRowcommand.Parameters.AddWithValue("@lacc_x", data[12]);
-                                            insertRowcommand.Parameters.AddWithValue("@lacc_y", data[13]);
-                                            insertRowcommand.Parameters.AddWithValue("@lacc_z", data[14]);
-                                            insertRowcommand.Parameters.AddWithValue("@quat_x", data[15]);
-                                            insertRowcommand.Parameters.AddWithValue("@quat_y", data[16]);
-                                            insertRowcommand.Parameters.AddWithValue("@quat_z", data[17]);
-                                            insertRowcommand.Parameters.AddWithValue("@quat_w", data[18]);
+                                        // Append values for each row
+                                        bulkInsertSql.Append($"({nextNumTest}, {id}, {item}, '{data[1]}', '{data[3]}', '{data[4]}', '{data[5]}', '{data[6]}', '{data[7]}', '{data[8]}', '{data[9]}', '{data[10]}', '{data[11]}', '{data[12]}', '{data[13]}', '{data[14]}', '{data[15]}', '{data[16]}', '{data[17]}', '{data[18]}'),");
+                                        item++;
+                                    }
 
-                                            insertRowcommand.ExecuteNonQuery();
-                                            item++;
-                                        }
+                                    // Remove the trailing comma
+                                    bulkInsertSql.Length -= 1;
+
+                                    using (MySqlCommand bulkInsertCommand = new MySqlCommand(bulkInsertSql.ToString(), connection.GetConnection()))
+                                    {
+                                        // Execute the bulk insert
+                                        bulkInsertCommand.ExecuteNonQuery();
                                     }
 
                                     // Commit the transaction
